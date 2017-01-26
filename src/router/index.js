2ea14a47ce57ca20a0827f1ceb453d9f01324229
Makcy <644215038@qq.com>
2017-01-27 01:28:02 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Login from '../views/login';
+import Index from '../views/index';
 
 
 Vue.use(Router);
@@ -8,6 +9,11 @@ const router = new Router({
   // history mode
   mode: 'hash',
   routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: Index,
+    },
     {
       path: '/login',
       name: 'login',
---
+++ src/views/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="wancai-index">
+    <h1>This is Index Page</h1>
+    <router-link to="/login">Login</router-link>
+  </div>
+</template>
+<script>
+  export default{
+    name: 'Index',
+  };
+</script>
