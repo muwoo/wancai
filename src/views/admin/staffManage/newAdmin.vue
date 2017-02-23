20652bd24b8f31dc38db03e1828e8189d71fc24d
Makcy <644215038@qq.com>
2017-02-23 23:42:19 +0800

    修改样式

--- src/views/admin/staffManage/newAdmin.vue
+++ src/views/admin/staffManage/newAdmin.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="newAdmin">
-    <el-form :model="adminInfo" :rules="adminInfoRules" ref="adminInfoForm">
+    <el-form :model="adminInfo" label-width="240px" :rules="adminInfoRules" ref="adminInfoForm">
       <el-form-item label='选择角色：' prop="role">
         <el-select v-model="adminInfo.role" placeholder="请选择">
           <el-option
@@ -10,7 +10,7 @@
           </el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="上传头像：" prop="avatar" label-width="300">
+      <el-form-item label="上传头像：" prop="avatar">
         <!-- <el-upload
           action="//jsonplaceholder.typicode.com/posts/"
           type="drag"
@@ -25,47 +25,47 @@
           <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
         </el-upload> -->
       </el-form-item>
-      <el-form-item label='手机号码：' prop="phoneNumber" label-width="300">
+      <el-form-item label='手机号码：' prop="phoneNumber">
         <el-input v-model="adminInfo.phoneNumber" placeholder="请输入内容" style="width: 240px;"></el-input>
       </el-form-item>
-      <el-form-item label='姓名：' prop="name" label-width="300">
+      <el-form-item label='姓名：' prop="name">
         <el-input v-model="adminInfo.name" placeholder="请输入内容" style="width: 150px;"></el-input>
       </el-form-item>
-      <el-form-item label='身份证号码：' prop="idCardNumber" label-width="300">
+      <el-form-item label='身份证号码：' prop="idCardNumber">
         <el-input v-model="adminInfo.idCardNumber" placeholder="请输入内容" style="width: 240px;" ></el-input>
       </el-form-item>
-      <el-form-item label="性别：" label-width="300">
+      <el-form-item label="性别：">
         <el-radio-group v-model="adminInfo.sex">
           <el-radio label="男"></el-radio>
           <el-radio label="女"></el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="生日：" label-width="300">
+      <el-form-item label="生日：">
         <el-date-picker
           v-model="adminInfo.birh"
           type="datetime"
           placeholder="选择日期时间">
         </el-date-picker>
       </el-form-item>
-      <el-form-item label='邮箱：' prop="mail" label-width="300">
+      <el-form-item label='邮箱：' prop="mail">
         <el-input v-model="adminInfo.mail" placeholder="请输入内容" style="width: 240px;"></el-input>
       </el-form-item>
-      <el-form-item label='QQ：' prop="qq" label-width="300">
+      <el-form-item label='QQ：' prop="qq">
         <el-input v-model="adminInfo.qq" placeholder="请输入内容" style="width: 240px;"></el-input>
       </el-form-item>
-      <el-form-item label='微信号：' prop="qq" label-width="300">
+      <el-form-item label='微信号：' prop="qq">
         <el-input v-model="adminInfo.weChatNumber" placeholder="请输入内容" style="width: 240px;"></el-input>
       </el-form-item>
-      <el-form-item label='固定电话：' prop="qq" label-width="300">
+      <el-form-item label='固定电话：' prop="qq">
         <el-input v-model="adminInfo.telephone" placeholder="请输入内容" style="width: 240px;"></el-input>
       </el-form-item>
-      <el-form-item label='虚拟短号：' prop="qq" label-width="300">
+      <el-form-item label='虚拟短号：' prop="qq">
         <el-input v-model="adminInfo.shordPhoneNubmer" placeholder="请输入内容" style="width: 240px;"></el-input>
       </el-form-item>
-      <el-form-item label='密码：' prop="qq" label-width="300">
+      <el-form-item label='密码：' prop="qq">
         <el-input v-model="adminInfo.password" placeholder="请输入内容" style="width: 240px;"></el-input>
       </el-form-item>
-      <el-form-item label='确认密码：' prop="qq" label-width="300">
+      <el-form-item label='确认密码：' prop="qq">
         <el-input v-model="adminInfo.confirmPass" placeholder="请输入内容" style="width: 240px;"></el-input>
       </el-form-item>
       <el-form-item>
--- src/views/admin/staffManage/newProjectManager.vue
+++ src/views/admin/staffManage/newProjectManager.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="newProjectManager">
-    <el-form :model="projectManagerInfo" :rules="projectManagerInfoRules" ref="projectManagerInfoForm">
-      <el-form-item label="上传头像：" prop="avatar" label-width="300">
+    <el-form :model="projectManagerInfo" label-width="240px" :rules="projectManagerInfoRules" ref="projectManagerInfoForm">
+      <el-form-item label="上传头像：" prop="avatar">
         <!-- <el-upload
           action="//jsonplaceholder.typicode.com/posts/"
           type="drag"
@@ -16,47 +16,47 @@
           <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
         </el-upload> -->
       </el-form-item>
-      <el-form-item label='手机号码：' prop="phoneNumber" label-width="300">
+      <el-form-item label='手机号码：' prop="phoneNumber">
         <el-input v-model="projectManagerInfo.phoneNumber" placeholder="请输入内容" style="width: 240px;"></el-input>
       </el-form-item>
-      <el-form-item label='姓名：' prop="name" label-width="300">
+      <el-form-item label='姓名：' prop="name">
         <el-input v-model="projectManagerInfo.name" placeholder="请输入内容" style="width: 150px;"></el-input>
       </el-form-item>
-      <el-form-item label='身份证号码：' prop="idCardNumber" label-width="300">
+      <el-form-item label='身份证号码：' prop="idCardNumber">
         <el-input v-model="projectManagerInfo.idCardNumber" placeholder="请输入内容" style="width: 240px;" ></el-input>
       </el-form-item>
-      <el-form-item label="性别：" label-width="300">
+      <el-form-item label="性别：">
         <el-radio-group v-model="projectManagerInfo.sex">
           <el-radio label="男"></el-radio>
           <el-radio label="女"></el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="生日：" label-width="300">
+      <el-form-item label="生日：" >
         <el-date-picker
           v-model="projectManagerInfo.birh"
           type="datetime"
           placeholder="选择日期时间">
         </el-date-picker>
       </el-form-item>
-      <el-form-item label='邮箱：' prop="mail" label-width="300">
+      <el-form-item label='邮箱：' prop="mail">
         <el-input v-model="projectManagerInfo.mail" placeholder="请输入内容" style="width: 240px;"></el-input>
       </el-form-item>
-      <el-form-item label='QQ：' prop="qq" label-width="300">
+      <el-form-item label='QQ：' prop="qq">
         <el-input v-model="projectManagerInfo.qq" placeholder="请输入内容" style="width: 240px;"></el-input>
       </el-form-item>
-      <el-form-item label='微信号：' prop="qq" label-width="300">
+      <el-form-item label='微信号：' prop="qq">
         <el-input v-model="projectManagerInfo.weChatNumber" placeholder="请输入内容" style="width: 240px;"></el-input>
       </el-form-item>
-      <el-form-item label='固定电话：' prop="qq" label-width="300">
+      <el-form-item label='固定电话：' prop="qq">
         <el-input v-model="projectManagerInfo.telephone" placeholder="请输入内容" style="width: 240px;"></el-input>
       </el-form-item>
-      <el-form-item label='虚拟短号：' prop="qq" label-width="300">
+      <el-form-item label='虚拟短号：' prop="qq">
         <el-input v-model="projectManagerInfo.shordPhoneNubmer" placeholder="请输入内容" style="width: 240px;"></el-input>
       </el-form-item>
-      <el-form-item label='密码：' prop="qq" label-width="300">
+      <el-form-item label='密码：' prop="qq">
         <el-input v-model="projectManagerInfo.password" placeholder="请输入内容" style="width: 240px;"></el-input>
       </el-form-item>
-      <el-form-item label='确认密码：' prop="qq" label-width="300">
+      <el-form-item label='确认密码：' prop="qq">
         <el-input v-model="projectManagerInfo.confirmPass" placeholder="请输入内容" style="width: 240px;"></el-input>
       </el-form-item>
       <el-form-item>
