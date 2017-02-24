b8811a5561b563d381de98d2cd0a01647aee7509
Makcy <644215038@qq.com>
2017-02-24 12:24:43 +0800

    fix

--- src/views/admin/staffManage/newRole.vue
+++ src/views/admin/staffManage/newRole.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="newRole">
-    <el-form ref="roleForm" :model="roleInfo" label-width="80px">
+    <el-form :model="roleInfo" label-width="240px" ref="roleForm">
       <el-form-item label="角色名称">
-        <el-input v-model="roleInfo.name"></el-input>
+        <el-input v-model="roleInfo.name" placeholder="请输入内容" style="width: 240px;"></el-input>
       </el-form-item>
       <el-form-item label="备注">
-        <el-input v-model="roleInfo.comment"></el-input>
+        <el-input v-model="roleInfo.comment" placeholder="请输入内容" style="width: 340px;"></el-input>
       </el-form-item>
       <el-form-item label="权限">
         <el-tree
@@ -14,7 +14,8 @@
           :load="loadNode"
           lazy
           show-checkbox
-          @check-change="handleCheckChange">
+          @check-change="handleCheckChange"
+          style="width: 250px;">
         </el-tree>
       </el-form-item>
     </el-form>
