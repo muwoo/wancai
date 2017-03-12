e9a782f9ef3050383ddb4255dbaf70f33e2a559c
Makcy <644215038@qq.com>
2017-03-12 16:17:00 +0800

    fix

---
+++ src/components/projectManagerInfo.vue
@@ -0,0 +1,162 @@
+<template>
+  <div id="projectManagerInfo">
+    <el-row class="top">
+      <el-col :span="4">ID：{{ projectManager.id }}</el-checkbox></el-col>
+      <el-col :span="8" :offset="12">
+        <el-row type="flex" justify="end" style="padding-right: 20px;">
+          创建时间：{{ projectManager.createdAt | formatDate }}
+        </el-row>
+      </el-col>
+    </el-row>
+    <el-row class="body">
+      <el-col :span="6">
+        <el-row class="image-col" type="flex" justify="space-around" align="middle">
+          <img class="image" v-for="item in projectManager.idCardImages" @click.prevent="handleClickImage" :src="item"/>
+        </el-row>
+      </el-col>
+      <el-col :span="7" :offset="1" class="content-eol ">
+        <el-row class="unit-row">{{ projectManager.name }}{{ projectManager.sex }}</el-row>
+        <el-row class="unit-row">{{ projectManager.idCard }}</el-row>
+        <el-row class="unit-row">{{ projectManager.birth }}</el-row>
+        <el-row class="unit-row">
+          <el-tooltip :content="projectManager.address" placement="top">
+            {{ limitAddress(projectManager.address) }}
+          </el-tooltip>
+        </el-row>
+      </el-col>
+      <el-col :span="5" :offset="1" class="content-eol">
+        <el-row class="unit-row">手机号码{{ projectManager.phoneNumber }}</el-row>
+        <el-row class="unit-row">邮箱：{{ projectManager.mail }}</el-row>
+        <el-row class="unit-row">QQ：{{ projectManager.qq }}</el-row>
+        <el-row class="unit-row">微信号：{{ projectManager.weChat }}</el-row>
+        <el-row class="unit-row">电话：{{ projectManager.telePhone }}</el-row>
+        <el-row class="unit-row">短号：{{ projectManager.shortPhone }}</el-row>
+      </el-col>
+      <el-col class="btn-row" :span="3" :offset="1">
+        <el-button type="primary" size="large" @click.prevent="handleEdit" style="width: 80px;">修 改</el-button>
+        <el-button type="danger" size="large" @click.prevent="handleDelete" style="width: 80px; margin-left: 0px;">删 除</el-button>
+      </el-col>
+    </el-row>
+    <!-- <el-dialog v-model="BigImageVisible" @close="handleBigImageClose">
+      <img class="big-img" :src="currentImage" />
+    </el-dialog> -->
+    <bigImage v-model="BigImageVisible" :image="currentImage" :visible="BigImageVisible" @handleWrapperClick="handleBigImageClose"></bigImage>
+  </div>
+</template>
+<script>
+  import bigImage from './bigImage';
+  import util from '../common/util';
+
+  export default {
+    name: 'projectManagerInfo',
+    data() {
+      return {
+        BigImageVisible: false,
+        currentImage: '',
+      };
+    },
+    props: [
+      'projectManager',
+    ],
+    components: {
+      bigImage,
+    },
+    methods: {
+      handleEdit(evt) {
+        this.$emit('handleEdit', evt);
+      },
+      handleDelete(evt) {
+        this.$emit('handleDelete', evt);
+      },
+      handleClickImage(evt) {
+        this.currentImage = evt.target.src;
+        this.BigImageVisible = true;
+      },
+      limitAddress(str) {
+        return `${str.substr(0, 20)}...`;
+      },
+      handleBigImageClose() {
+        this.BigImageVisible = false;
+      },
+    },
+    filters: {
+      formatDate(time) {
+        const date = new Date(parseInt(time, 0));
+        return util.formatDate.format(date, 'yyyy-MM-dd hh:mm');
+      },
+    },
+  };
+</script>
+<style lang="scss" scoped>
+#projectManagerInfo {
+ width: 100%;
+ height: 190px;
+ // margin-left: 10px;
+ // margin-right: 10px;
+ // padding-right: 10px;
+ background-color: #FFFFFF;
+ .top {
+   height: 40px;
+   background-color: #eff2f7;
+   border:1px solid #FFFFFF;
+
+   .el-col {
+     height: 100%;
+     line-height: 40px;
+     font-size: 14px;
+   }
+   .el-checkbox {
+     width: 100%;
+     height: 100%;
+     padding-left: 10px;
+   }
+ }
+ .body {
+   height: 150px;
+  //  background-color: pink;
+   .el-col {
+     height: 100%;
+     font-size: 14px;
+    //  line-height: 40px;
+   }
+   .image-col {
+     height: 100%;
+     width: 100%;
+    //  padding-top: 10px;
+     .image {
+       width: 40%;
+       height: 50px;
+     }
+   }
+   .unit-row {
+     height: 15px;
+     line-height: 15px;
+     margin-top: 5px;
+   }
+   .content-eol {
+     display: flex;
+     flex-direction: column;
+     align-items: start;
+     justify-content: center;
+   }
+   .btn-row {
+     display: flex;
+     flex-direction: column;
+     align-items: center;
+     justify-content: center;
+
+     > button + button {
+       margin-top: 10px;
+     }
+   }
+  //  .el-dialog {
+  //    width: 500px;
+  //    height: 300px;
+  //  }
+   .big-img {
+     width: 500px;
+     height: 300px;
+   }
+ }
+}
+</style>
--- src/views/admin/project_manager/list.vue
+++ src/views/admin/project_manager/list.vue
@@ -1,10 +1,90 @@
 <template>
-  <p>项目经理管理</p>
+  <div id="projectManagerList">
+    <el-form :inline="true" :model="projectManagerInfo" style="margin-top: 20px;">
+      <el-row>
+        <el-col :span="24">
+          <el-col :span="8">
+            <el-form-item label='身份证号：' style="width: 100%;">
+              <el-input v-model="projectManagerInfo.idCard" placeholder="请输入内容" style="width: 150px;"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label='手机号码：' style="width: 100%;">
+              <el-input v-model="projectManagerInfo.phoneNumber" placeholder="请输入内容" style="width: 150px;"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+          <el-form-item label='姓名：' style="width: 100%;">
+              <el-input v-model="projectManagerInfo.name" placeholder="请输入内容" style="width: 150px;"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-col>
+      </el-row>
+      <el-form-item>
+        <el-button type="primary" @click="handleSearchItem">搜索名单</el-button>
+      </el-form-item>
+    </el-form>
+    <div class="tips"></div>
+    <div class="card-panel">
+      <projectManagerInfo v-for="info in infos" :projectManager="info"
+      style="margin-top: 10px;" ></projectManagerInfo>
+    </div>
+  </div>
 </template>
 <script>
+  import projectManagerInfo from '../../../components/projectManagerInfo';
+
   export default {
     name: 'projectManagerList',
+    components: {
+      projectManagerInfo,
+    },
+    data() {
+      return {
+        projectManagerInfo: {
+          idCard: '',
+          phoneNumber: '',
+          name: '',
+        },
+        infos: [{
+          id: 111,
+          createdAt: 1487218088000,
+          idCardImages: [
+            'http://img1.imgtn.bdimg.com/it/u=2173638125,1490913710&fm=15&gp=0.jpg',
+            'http://img3.imgtn.bdimg.com/it/u=1951674198,2294779761&fm=23&gp=0.jpg',
+          ],
+          name: '赵日天',
+          sex: '男',
+          nation: '汉族',
+          idCard: '352341233214112232',
+          birth: '91年4月',
+          address: '浙江省杭州市西湖区文三西路999号',
+          phoneNumber: '13111111111',
+          mail: '123456@qq.com',
+          qq: '123456',
+          weChat: 'librazhoux',
+          telePhone: '183000000',
+          shortPhone: '45123',
+        }],
+      };
+    },
+    methods: {
+      handleSearchItem() {
+
+      },
+    },
   };
 </script>
 <style lang="scss" scoped>
+#projectManagerList {
+  .tips {
+    border-bottom: 1px solid #d3dce6;
+  }
+  .card-panel {
+    width: 100%;
+    height: 800px;
+    // overflow: hidden;
+    // overflow-y: scroll;
+  }
+}
 </style>
