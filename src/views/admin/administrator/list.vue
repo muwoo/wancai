a8d28f6489da0d5153297c38b22356befeb3f7ae
Makcy <644215038@qq.com>
2017-03-18 15:35:08 +0800

    添加管理员管理和新建角色

--- src/views/admin/administrator/list.vue
+++ src/views/admin/administrator/list.vue
@@ -1,10 +1,119 @@
 <template>
-  <p>管理员管理</p>
+  <div id="administratorList">
+    <el-form :inline="true" :model="adminInfo" style="margin-top: 20px;">
+      <el-row>
+        <el-col :span="24">
+          <el-col :span="8">
+            <el-form-item label='身份证号：' style="width: 100%;">
+              <el-input v-model="adminInfo.idCard" placeholder="请输入内容" style="width: 150px;"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label='手机号码：' style="width: 100%;">
+              <el-input v-model="adminInfo.phoneNumber" placeholder="请输入内容" style="width: 150px;"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label='姓名：' style="width: 100%;">
+                <el-input v-model="adminInfo.name" placeholder="请输入内容" style="width: 150px;"></el-input>
+            </el-form-item>
+        </el-col>
+        </el-col>
+      </el-row>
+      <el-form-item>
+        <el-button type="primary" @click="handleSearchItem">搜索名单</el-button>
+      </el-form-item>
+    </el-form>
+    <div class="tips"></div>
+    <div class="card-panel"
+      v-loading="loading">
+      <administratorInfo v-for="info in infos" :projectManager="info"
+      @handleEdit="handleEdit(this.event, info)"
+      @handleDelete="handleDelete(this.event, info)"
+      style="margin-top: 10px;" ></adminInfo>
+      <el-col :span="24" style="margin-top:10px;">
+        <el-pagination layout="prev, pager, next" @current-change="handleCurrentPageChange" :current-page="currentPage" :page-count="pageCount" style="float: right;"></el-pagination>
+      </el-col>
+    </div>
+  </div>
 </template>
 <script>
+  import administratorInfo from '../../../components/administratorInfo';
+
   export default {
     name: 'administratorList',
+    components: {
+      administratorInfo,
+    },
+    data() {
+      return {
+        adminInfo: {
+          idCard: '',
+          phoneNumber: '',
+          name: '',
+        },
+        infos: [],
+        currentPage: 1,
+        pageSize: 20,
+        pageCount: 0,
+        totalProjectSize: 100,
+        loading: false,
+      };
+    },
+    methods: {
+      handleEdit(event, obj) {
+        console.log(obj);
+      },
+      handleDelete(event, obj) {
+        console.log(event);
+      },
+      handleSearchItem() {
+        this.getProjectManagers();
+      },
+      handleCurrentPageChange(val) {
+        this.currentPage = val;
+        this.getProjectManagers();
+      },
+      getProjectManagers() {
+        this.loading = true;
+        const params = {
+          pageNum: this.currentPage,
+          pageSize: this.pageSize,
+          idCard: this.adminInfo.idCard,
+          phone: this.adminInfo.phone,
+          name: this.adminInfo.name,
+        };
+        // console.log(params);
+        this.$http.post('/manager/list', params).then((response) => {
+          const {
+            data: {
+              list, pages, total, pageNum,
+            },
+          } = response.data;
+          this.totalProjectSize = total;
+          this.currentPage = pageNum;
+          this.pageCount = pages;
+          this.infos = list;
+          this.loading = false;
+        }).catch((error) => {
+          console.log(error);
+          this.loading = false;
+        });
+      },
+    },
+    mounted() {
+      this.getProjectManagers();
+    },
   };
 </script>
 <style lang="scss" scoped>
+#administratorList {
+  .tips {
+    border-bottom: 1px solid #d3dce6;
+  }
+  .card-panel {
+    width: 100%;
+    height: auto;
+  }
+}
 </style>
--- src/views/admin/administrator/new_role.vue
+++ src/views/admin/administrator/new_role.vue
@@ -1,10 +1,59 @@
 <template>
-  <p>新建角色</p>
+  <div id="administratorNewRole">
+    <el-form :model="roleInfo" label-width="240px" ref="roleForm">
+      <el-form-item label="角色名称">
+        <el-input v-model="roleInfo.name" placeholder="请输入内容" style="width: 240px;"></el-input>
+      </el-form-item>
+      <el-form-item label="备注">
+        <el-input v-model="roleInfo.comment" placeholder="请输入内容" style="width: 340px;"></el-input>
+      </el-form-item>
+      <el-form-item label="权限">
+        <el-tree
+          :data="authItems"
+          :props="props"
+          :load="loadNode"
+          lazy
+          show-checkbox
+          @check-change="handleCheckChange"
+          style="width: 250px;">
+        </el-tree>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
+
 <script>
-  export default {
-    name: 'administratorNewRole',
-  };
+export default {
+  name: 'administratorNewRole',
+  data() {
+    return {
+      roleInfo: {
+        name: '',
+        comment: '',
+        auth: '',
+      },
+      authItems: [{
+        name: '经纪人管理',
+      },
+      {
+        name: '管理员管理',
+      }],
+      props: {
+        label: 'name',
+        children: 'zones',
+      },
+    };
+  },
+  methods: {
+    handleCheckChange() {
+    },
+    loadNode(node, resolve) {
+    },
+  },
+};
 </script>
+
 <style lang="scss" scoped>
+  #administratorNewRole {
+  }
 </style>
