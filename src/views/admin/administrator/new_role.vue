a1efa3dcbe5b82996da6e3789a164e7cb082e1fe
Makcy <644215038@qq.com>
2017-04-02 00:21:34 +0800

    fix

--- src/views/admin/administrator/new_role.vue
+++ src/views/admin/administrator/new_role.vue
@@ -5,7 +5,7 @@
         <el-input v-model="roleInfo.name" placeholder="请输入内容" style="width: 240px;"></el-input>
       </el-form-item>
       <el-form-item label="备注">
-        <el-input v-model="roleInfo.comment" placeholder="请输入内容" style="width: 340px;"></el-input>
+        <el-input v-model="roleInfo.description" placeholder="请输入内容" style="width: 340px;"></el-input>
       </el-form-item>
       <el-form-item label="权限">
         <el-tree
@@ -34,7 +34,7 @@ export default {
     return {
       roleInfo: {
         name: '',
-        comment: '',
+        description: '',
         auth: '',
       },
       authItems: [],
@@ -49,7 +49,40 @@ export default {
   },
   methods: {
     handleSubmit() {
-      console.log(this.$refs.tree.getCheckedKeys());
+      const params = {
+        id: this.$route.params.id,
+        roleName: this.roleInfo.name,
+        description: this.roleInfo.description,
+        arr: this.getFinalCheckKeys(),
+      };
+      this.$http.post('/admin/role/add', params).then((response) => {
+        if (response.data.errorCode === 10000) {
+          this.$notify({
+            title: '新建成功',
+            type: 'success',
+          });
+          this.$router.push('role_list');
+        }
+      }).catch((error) => {
+        console.log(error);
+      });
+    },
+    getFinalCheckKeys(array) {
+      // eslint-disable-next-line
+      const childArray = this.$refs.tree.getCheckedNodes().map((node) => {
+        return node.id;
+      });
+      // eslint-disable-next-line
+      const parentArray = this.$refs.tree.getCheckedNodes().map((node) => {
+        return node.parentId;
+      });
+
+      const finalArray = childArray.concat(parentArray);
+      const uniqFinalArray = Array.from(new Set(finalArray));
+      // eslint-disable-next-line
+      return uniqFinalArray.filter((arr) => {
+        return arr !== 0;
+      });
     },
     getParentKeys(array) {
       this.defaultExpandedKeys = [];
