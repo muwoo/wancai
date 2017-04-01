378ae6e701d492963935c917b60efddc2d51571a
Makcy <644215038@qq.com>
2017-04-01 21:29:35 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,12 @@
 <script>
 import Vue from 'vue';
 import Element from 'element-ui';
-import AMap from 'vue-amap';
+import BaiduMap from 'vue-baidu-map';
 import 'element-ui/lib/theme-default/index.css';
 import router from './router';
 
-Vue.use(AMap);
-AMap.initAMapApiLoader({
-  key: '6bcd28bf614c37b23cbe45d3a74096ba',
-  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+Vue.use(BaiduMap, {
+  ak: 'joyrbPIrOPbfyPHIjFW4bnEtu0Cee3P9',
 });
 Vue.use(Element);
 
