b12b016eff27c235ec5e3b0a3c9a08a7ecf9d5f3
Makcy <644215038@qq.com>
2017-04-02 14:28:26 +0800

    add password find

---
+++ src/views/admin/password_find.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="adminPasswordFind">
+    <p>管理员忘记密码</p>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'adminPasswordFind',
+  };
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/views/projectManager/password_find.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="managerPasswordFind">
+    <p>管理员忘记密码</p>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'managerPasswordFind',
+  };
+</script>
+<style lang="scss" scoped>
+</style>
