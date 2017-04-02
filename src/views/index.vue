d64ade676daca68307f82c2ea2d4745b44f18ce3
Makcy <644215038@qq.com>
2017-04-02 14:27:58 +0800

    adjuest index

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,10 +2,10 @@
   <div id="wancai-index">
     <h1>This is Index Page</h1>
     <div>
-      <router-link :to="{ name: 'projectManagerLogin' }">ProjectManager_Login</router-link>
+      <router-link :to="{ name: 'projectManagerLogin' }">项目经理登录</router-link>
     </div>
     <div>
-      <router-link :to="{ name: 'adminLogin' }">Admin_Login</router-link>
+      <router-link :to="{ name: 'adminLogin' }">管理员登录</router-link>
     </div>
   </div>
 </template>
