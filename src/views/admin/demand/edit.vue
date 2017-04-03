a627a49966c0007e6eb0ad16c454cc407b59ffb3
Makcy <644215038@qq.com>
2017-04-04 01:06:35 +0800

    fixgst

--- src/views/admin/demand/edit.vue
+++ src/views/admin/demand/edit.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="adminDemandEdit" v-loading="loading">
-    <el-form ref="applyDemandForm" label-width="100px">
+    <!-- 需求表表单 -->
+    <el-form :model="demandInfo" ref="demandInfoForm" :rules="demandInfoRules" label-width="100px">
       <h1 class="tips">需求表</h1>
-      <el-form-item label="表名：" style="width: 400px;">
+      <el-form-item prop="title" label="表名：" style="width: 400px;">
         <el-input v-model="demandInfo.title" placeholder="请输入内容"></el-input>
       </el-form-item>
-      <!-- <el-form-item label="类型：">
+      <!-- <el-form-item prop="type" label="类型：">
         <el-radio-group v-model="demandInfo.type">
           <el-radio :label="0">兼职</el-radio>
           <el-radio :label="1">全职</el-radio>
         </el-radio-group>
       </el-form-item> -->
-      <el-form-item label="岗位：" style="width: 300px;">
+      <el-form-item prop="job" label="岗位：" style="width: 300px;">
         <el-input v-model="demandInfo.job" placeholder="请输入内容"></el-input>
       </el-form-item>
-      <el-form-item label="申请原因：">
+      <el-form-item prop="applyReason" label="申请原因：">
         <el-radio-group v-model="demandInfo.applyReason">
           <el-radio :label="0">离职补缺</el-radio>
           <el-radio :label="1">新增职位</el-radio>
@@ -24,7 +25,8 @@
       </el-form-item>
     </el-form>
     <h1 class="tips">岗位信息</h1>
-    <el-form v-if="demandInfo.type === 1" refs="applyDemandForm" :model="demandInfo" label-width="100px">
+    <!-- 全职表单 -->
+    <el-form v-if="demandInfo.type === 1" refs="fullTimeForm" :rules="fullTimeRules" :model="demandInfo" label-width="100px">
       <el-form-item label="工种：" style="width: 400px;">
         <el-select v-model="demandInfo.workType" placeholder="请选择">
          <el-option
@@ -34,10 +36,10 @@
          </el-option>
        </el-select>
       </el-form-item>
-      <el-form-item label="薪资：" style="width: 400px;">
+      <el-form-item prop="salary" label="薪资：" style="width: 400px;">
         <el-input v-model="demandInfo.salary" placeholder="请输入内容"></el-input>
       </el-form-item>
-      <el-form-item label="结算方式：" style="width: 400px;">
+      <el-form-item prop="settlementType" label="结算方式：" style="width: 400px;">
         <el-select v-model="demandInfo.settlementType" placeholder="请选择">
           <el-option
             v-for="item in moneyClearType"
@@ -46,27 +48,48 @@
           </el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="需求人数：" style="width: 400px;">
+      <el-form-item prop="applyNumber" label="需求人数：" style="width: 400px;">
          <el-input-number v-model="demandInfo.applyNumber" :min="0" :max="10000"></el-input-number>
       </el-form-item>
-      <el-form-item label="到岗时间：" style="width: 400px;">
+      <el-form-item prop="workTime" label="到岗时间：" style="width: 400px;">
         <el-input v-model="demandInfo.workTime" placeholder="请输入内容"></el-input>
       </el-form-item>
-      <el-form-item v-for="interview in demandInfo.listDemandInterview" label="面试时间：">
+      <el-form-item v-for="(interview, index) in demandInfo.listDemandInterview" label="面试时间：">
         <el-col :span="9">
           <el-input v-model="interview.interviewTime" placeholder="请输入内容"></el-input>
         </el-col>
         <el-col :span="3" style="text-align: right;">面试地点：</el-col>
-        <el-col :span="12">
-          <el-input v-model="interview.interviewAddress" placeholder="请输入内容"></el-input>
+        <el-col :span="6">
+          <el-input v-model="interview.interviewAddress" placeholder="请在地图上进行选择"></el-input>
+        </el-col>
+        <el-col :span="3" :offset="1">
+          <el-button v-if="!isMapShow" @click="handleShowMap(this.event, index)">展开地图</el-button>
+          <el-button v-if="isMapShow" @click="handleShowMap(this.event, index)">收起地图</el-button>
         </el-col>
       </el-form-item>
-      <el-form-item>
+      <div v-if="isMapShow">
+        <el-input v-model="mapConfig.keyword" placeholder="搜索地点，并进行选择" style="width: 440px; margin-bottom: 5px;"></el-input>
+        <baidu-map class="map-container"
+        :scroll-wheel-zoom="true"
+        :center="mapConfig.center"
+        :zoom="mapConfig.zoom">
+          <bm-view class="bm-view">
+          </bm-view>
+          <bm-local-search
+            :keyword="mapConfig.keyword"
+            :auto-viewport="true"
+            :location="mapConfig.location"
+            @infohtmlset="infohtmlset">
+          </bm-local-search>
+        </baidu-map>
+      </div>
+      <el-form-item v-if="!isMapShow">
         <el-button type="text" @click="handleAddInterview">+添加面试安排</el-button>
         <el-button v-if="demandInfo.listDemandInterview.length > 1" type="text" @click="handleDelInterview">-删减面试安排</el-button>
       </el-form-item>
     </el-form>
-    <el-form v-if="demandInfo.type === 0" ref="applyDemandForm" :model="demandInfo" label-width="100px">
+    <!-- 兼职表单 -->
+    <el-form v-if="demandInfo.type === 0" ref="partTimeForm" :rules="partTimeRules" :model="demandInfo" label-width="100px">
       <el-form-item label="工种：" style="width: 400px;">
         <el-select v-model="demandInfo.workType" placeholder="请选择">
          <el-option
@@ -76,7 +99,7 @@
          </el-option>
        </el-select>
       </el-form-item>
-      <el-form-item label="薪资：" style="width: 400px;">
+      <el-form-item prop="salary" label="薪资：" style="width: 400px;">
         <el-input v-model="demandInfo.salary" placeholder="请输入内容"></el-input>
       </el-form-item>
       <el-form-item label="结算方式：" style="width: 400px;">
@@ -115,7 +138,8 @@
       </el-form-item>
     </el-form>
     <h1 class="tips">岗位要求</h1>
-    <el-form ref="applyDemandForm" :model="demandInfo" label-width="100px">
+    <!-- 岗位要求表单 -->
+    <el-form ref="requirementForm" :rules="requirementRules" :model="demandInfo" label-width="100px">
       <el-form-item label="性别：">
         <el-radio-group v-model="demandInfo.sex">
           <el-radio :label="2">不限</el-radio>
@@ -145,7 +169,7 @@
            </el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="专业：" style="width: 400px;">
+      <el-form-item prop="profession" label="专业：" style="width: 400px;">
         <el-input v-model="demandInfo.profession" placeholder="请输入内容"></el-input>
       </el-form-item>
       <el-form-item label="婚否：">
@@ -164,7 +188,7 @@
            </el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="要求内容：">
+      <el-form-item prop="request" label="要求内容：">
         <el-input
           type="textarea"
           autosize
@@ -175,11 +199,9 @@
       </el-form-item>
     </el-form>
     <h1 class="tips"></h1>
-    <el-row>
-      <el-col :span="11" type="flex" align="center">
-        <el-button type="primary" @click="handleSubmit" :loading="publishing">确认修改</el-button>
-      </el-col>
-    </el-row>
+    <el-col :span="11" type="flex" align="center">
+      <el-button type="primary" @click="handleSubmit" :loading="publishing">提交申请</el-button>
+    </el-col>
   </div>
 </template>
 <script>
@@ -192,16 +214,16 @@
           title: '',
           job: '',
           applyReason: 0,
-          workType: '1',
-          settlementType: '1',
+          workType: 1,
+          settlementType: 1,
           salary: '',
           applyNumber: 0,
-          education: '1',
+          education: 1,
           profession: '',
           minAge: 18,
           maxAge: 100,
           personNum: 0,
-          workExperience: '1',
+          workExperience: 1,
           request: '',
           isMarry: 2,
           sex: 2,
@@ -217,8 +239,15 @@
             applyNumber: 0,
           }],
         },
+        mapConfig: {
+          location: '',
+          keyword: '',
+          center: { lng: 121.59996, lat: 31.197646 },
+          zoom: 12,
+        },
+        currentMapIndex: 0,
         publishing: false,
-        loading: true,
+        isMapShow: false,
         staffType: [{
           value: 1,
           label: '快递员',
@@ -336,6 +365,44 @@
             label: '10年以上',
             value: 7,
           }],
+        demandInfoRules: {
+          title: [
+            { required: true, message: '请输入需求表名', trigger: 'blur' },
+          ],
+          job: [
+            { required: true, message: '请输入岗位内容', trigger: 'blur' },
+          ],
+          // type: [
+          //   { required: true, message: '请选择岗位类型', trigger: 'blur' },
+          // ],
+          // applyReason: [
+          //   { required: true, message: '请选择申请原因', trigger: 'blur' },
+          // ],
+        },
+        partTimeRules: {
+          salary: [
+            { required: true, message: '请输入薪水', trigger: 'blur' },
+          ],
+        },
+        fullTimeRules: {
+          salary: [
+            { required: true, message: '请输入薪水', trigger: 'blur' },
+          ],
+          applyNumber: [
+            { required: true, message: '请输入需求人数', trigger: 'blur' },
+          ],
+          workTime: [
+            { required: true, message: '请输入到岗时间', trigger: 'blur' },
+          ],
+        },
+        requirementRules: {
+          profession: [
+            { required: true, message: '请输入专业名称', trigger: 'blur' },
+          ],
+          request: [
+            { required: true, message: '请输入要求内容', trigger: 'blur' },
+          ],
+        },
       };
     },
     methods: {
@@ -356,53 +423,84 @@
         }
       },
       handleSubmit() {
-        const demandId = this.$route.params.id;
-        this.$refs.applyDemandForm.validate((valid) => {
-          if (valid) {
-            this.publishing = false;
-            const params = {
-              // projectId: this.$route.query.id,
-              id: demandId,
-              title: this.demandInfo.title,
-              type: this.demandInfo.type,
-              job: this.demandInfo.job,
-              applyReason: this.demandInfo.applyReason,
-              workType: this.demandInfo.workType,
-              settlementType: this.demandInfo.settlementType,
-              salary: this.demandInfo.salary,
-              applyNumber: this.demandInfo.applyNumber,
-              education: this.demandInfo.education,
-              profession: this.demandInfo.profession,
-              workExperience: this.demandInfo.workExperience,
-              request: this.demandInfo.request,
-              sex: this.demandInfo.sex,
-              maxAge: this.demandInfo.maxAge,
-              minAge: this.demandInfo.minAge,
-              workTime: this.demandInfo.workTime,
-              isMarry: this.demandInfo.isMarry,
-              listDemandInterview: this.demandInfo.listDemandInterview,
-              listSchedulingInformation: this.demandInfo.listSchedulingInformation,
-            };
-            this.$http.post('/demand/modify', params).then((response) => {
-              const { error, errorCode } = response.data;
-              if (errorCode === 10000) {
-                this.$notify({
-                  title: '修改成功',
-                  type: 'success',
-                });
-                this.$router.push({ name: 'adminDemandShow', params: { id: this.$route.params.id } });
-              } else {
-                this.$notify.error({
-                  title: '修改异常',
-                  type: 'success',
-                });
+        this.$refs.demandInfoForm.validate((demandValid) => {
+          if (demandValid) {
+            this.$refs.requirementForm.validate((reqValid) => {
+              if (reqValid) {
+                if (this.demandInfo.type === 1) {
+                  this.$refs.fullTimeForm.validate((fullValid) => {
+                    if (fullValid) {
+                      this.handlePublish();
+                    }
+                    return false;
+                  });
+                } else if (this.demandInfo.type === 0) {
+                  this.$refs.partTimeForm.validate((partValid) => {
+                    if (partValid) {
+                      this.handlePublish();
+                    }
+                    return false;
+                  });
+                }
               }
-            }).catch((error) => {
-              this.publish = false;
+              return false;
             });
           }
           return false;
         });
+      },
+      handlePublish() {
+        const demandId = this.$route.params.id;
+        this.publishing = false;
+        const params = {
+          id: demandId,
+          projectId: this.$route.query.id,
+          title: this.demandInfo.title,
+          type: this.demandInfo.type,
+          job: this.demandInfo.job,
+          applyReason: this.demandInfo.applyReason,
+          workType: this.demandInfo.workType,
+          settlementType: this.demandInfo.settlementType,
+          salary: this.demandInfo.salary,
+          applyNumber: this.demandInfo.applyNumber,
+          education: this.demandInfo.education,
+          profession: this.demandInfo.profession,
+          workExperience: this.demandInfo.workExperience,
+          request: this.demandInfo.request,
+          sex: this.demandInfo.sex,
+          maxAge: this.demandInfo.maxAge,
+          minAge: this.demandInfo.minAge,
+          workTime: this.demandInfo.workTime,
+          isMarry: this.demandInfo.isMarry,
+          listDemandInterview: this.demandInfo.listDemandInterview,
+          listSchedulingInformation: this.demandInfo.listSchedulingInformation,
+        };
+        this.$http.post(`${this.$managerURL}/demand/add`, params).then((response) => {
+          if (response.data.errorCode === 10000) {
+            this.$notify({
+              title: '发布成功',
+              type: 'success',
+            });
+            this.$router.push({ path: 'demand/pending', query: { id: this.$route.query.id } });
+          } else {
+            this.$notify.error({
+              title: '发布异常',
+              type: 'success',
+            });
+          }
+        }).catch((error) => {
+          console.log(error);
+        });
+      },
+      handleShowMap(event, index) {
+        this.isMapShow = !this.isMapShow;
+        this.currentMapIndex = index;
+      },
+      infohtmlset(poi) {
+        this.demandInfo.listDemandInterview[this.currentMapIndex].interviewAddress = `${poi.city}${poi.address}${poi.title}`;
+        this.demandInfo.listDemandInterview[this.currentMapIndex].latitude = poi.point.lat;
+        this.demandInfo.listDemandInterview[this.currentMapIndex].longitude = poi.point.lng;
+        this.isMapShow = false;
       },
     },
     filters: {
@@ -419,7 +517,7 @@
         this.loading = false;
       });
     },
-  };
+};
 </script>
 <style lang="scss" scoped>
 #adminDemandEdit {
